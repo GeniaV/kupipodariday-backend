0b11b482ea2dab369778a39eee4a20895ae724c8
Genia V <evgenia.verhovtseva@yandex.ru>
2023-02-06 23:07:33 +0300

    роуты для поиска заявок

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Header, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RequestWithUser } from 'src/types/types';
 import { CreateOfferDto } from './dto/create-offer.dto';
@@ -13,5 +22,15 @@ export class OffersController {
   @Header('Content-Type', 'application/json')
   create(@Body() createOfferDto: CreateOfferDto, @Req() req: RequestWithUser) {
     return this.offersService.create(req.user, createOfferDto);
+  }
+
+  @Get()
+  getOffers() {
+    return this.offersService.getOffers();
+  }
+
+  @Get(':id')
+  async getOfferById(@Param('id') id: string) {
+    return this.offersService.getOfferById(id);
   }
 }
--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { WishesService } from 'src/wishes/wishes.service';
-import { Repository } from 'typeorm';
+import { FindManyOptions, FindOneOptions, Repository } from 'typeorm';
 import { CreateOfferDto } from './dto/create-offer.dto';
 import { Offer } from './entities/offer.entity';
 
@@ -29,5 +29,53 @@ export class OffersService {
       item: wish,
     });
     return this.offersRepository.save(newOffer);
+  }
+
+  async findOne(query: FindOneOptions<Offer>): Promise<Offer> {
+    return this.offersRepository.findOne(query);
+  }
+
+  findMany(query: FindManyOptions<Offer>) {
+    return this.offersRepository.find(query);
+  }
+
+  async getOffers() {
+    const offersArr = await this.findMany({
+      relations: {
+        item: { offers: true, owner: true },
+        user: {
+          offers: { item: true },
+          wishes: { offers: true, owner: true },
+          wishlists: true,
+        },
+      },
+    });
+
+    offersArr.forEach((offer) => {
+      offer.amount = Number(offer.amount);
+      offer.item.price = Number(offer.item.price);
+      offer.user.wishes.forEach((wish) => (wish.price = Number(wish.price)));
+    });
+
+    return offersArr;
+  }
+
+  async getOfferById(id: string) {
+    const offer = await this.offersRepository.findOne({
+      where: [{ id: +id }],
+      relations: {
+        item: { offers: true, owner: true },
+        user: { offers: true, wishes: true, wishlists: true },
+      },
+    });
+    if (!offer) {
+      throw new NotFoundException();
+    }
+
+    offer.amount = Number(offer.amount);
+
+    offer.item.price = Number(offer.item.price);
+
+    return offer;
   }
 }
